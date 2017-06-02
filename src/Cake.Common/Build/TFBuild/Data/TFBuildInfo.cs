c9b247eefffa659f777dfb906cac206439e760ec
Rodney Littles II <rodney.littles@gmail.com>
2017-06-02 09:06:59 -0500

    Added additional TF build environment variables

--- src/Cake.Common/Build/TFBuild/Data/TFBuildInfo.cs
+++ src/Cake.Common/Build/TFBuild/Data/TFBuildInfo.cs
@@ -12,6 +12,8 @@ namespace Cake.Common.Build.TFBuild.Data
     /// </summary>
     public sealed class TFBuildInfo : TFInfo
     {
+        // TODO: https://www.visualstudio.com/en-us/docs/build/define/variables
+
         /// <summary>
         /// Initializes a new instance of the <see cref="TFBuildInfo"/> class.
         /// </summary>
@@ -70,5 +72,38 @@ namespace Cake.Common.Build.TFBuild.Data
         /// The email of the user the build was requested for.
         /// </value>
         public string RequestedForEmail => GetEnvironmentString("BUILD_REQUESTEDFOREMAIL");
+
+        /// <summary>
+        /// Gets the local path on the agent where any artifacts are copied to before being pushed to their destination. 
+        /// </summary>
+        /// <example><c>c:\agent\_work\1\a</c></example>
+        /// <value>
+        /// The artifact staging directory.
+        /// </value>
+        public string ArtifactStagingDirectory => GetEnvironmentString("BUILD_ARTIFACTSTAGINGDIRECTORY");
+
+        /// <summary>
+        /// Gets local path on the agent you can use as an output folder for compiled binaries.
+        /// </summary>
+        /// <value>
+        /// The binaries directory.
+        /// </value>
+        public string BinariesDirectory => GetEnvironmentString("BUILD_BINARIESDIRECTORY");
+
+        /// <summary>
+        /// Gets the name of the build definition..
+        /// </summary>
+        /// <value>
+        /// The definition.
+        /// </value>
+        public string Definition => GetEnvironmentString("BUILD_DEFINITIONNAME");
+
+        /// <summary>
+        /// Gets the version of the build definition.
+        /// </summary>
+        /// <value>
+        /// The definition version.
+        /// </value>
+        public string DefinitionVersion => GetEnvironmentString("BUILD_DEFINITIONVERSION");
     }
 }
